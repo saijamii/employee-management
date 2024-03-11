6dcecc64a37bfae3bd58338ec33e55f92182c0d3
sailakshmi <lakshmi@unosimple.com>
2024-03-11 22:50:06 +0530

    just for testing bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
   const appVersion = "v11.03.24.06";
   console.log(appVersion, "appVersion");
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>employee-management</p>
         <span>{appVersion}</span>
@@ -19,7 +20,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Layout />
     </div>
   );
 }
