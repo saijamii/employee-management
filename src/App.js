4d083398d14d1bcf5ae7ff2c76e9c6cf75e94bac
saijamii <saijami38@gmail.com>
2024-03-13 20:32:46 +0530

    appVersion added at Sider

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import Report from "./Pages/Report/Report";
 import Login from "./Pages/Login/Login.js";
 
 function App() {
-  const appVersion = "v13.03.24.02";
-  console.log(appVersion, "appVersion");
   return (
     <Router>
       <Layout>
