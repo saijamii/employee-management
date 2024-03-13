4fe3de55d3aef858b333f9e7d3a3573406f3c4b5
saijamii <saijami38@gmail.com>
2024-03-13 20:26:58 +0530

    Terminal Chanegs

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,33 +1,33 @@
-<<<<<<< HEAD
+
 import React, { useState } from "react";
-=======
-import React from "react";
->>>>>>> f766427 (leaves component)
 import { Route, Routes, Link} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
-import styles from '../StyleSheet/Sidebar.module.css'
+import styles from "../StyleSheet/Sidebar.module.css";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
 const Layout = () => {
-<<<<<<< HEAD
   const [menuItem, setMenuItem] = useState('Dashboard');
   const itemClickHandler = (itemName) => {
     console.log(itemName)
     setMenuItem(itemName)
   }
-=======
-    // const location = useLocation();
->>>>>>> f766427 (leaves component)
+
   return (
     <>
-      <div className="container-fluid" style={{backgroundColor:'#f0f2f5',minHeight:'100vh'}}>
-        <Header menuItem={menuItem}/>
+      <div
+        className="container-fluid"
+        style={{ backgroundColor: "#f0f2f5", minHeight: "100vh" }}
+      >
+        <Header menuItem={menuItem} />
         <div className="row flex-nowrap">
-          <div className={`${styles.sidebar} col-auto col-md-2 px-1 px-sm-2 px-0 bg-dark`}>
-           <Sidebar itemClickHandler={itemClickHandler}/>
-          </div>                   
-          <div className="col-md-10 offset-md-2 py-5">            
+          <div
+            className={`${styles.sidebar} col-auto col-md-2 px-1 px-sm-2 px-0 bg-dark`}
+          >
+            <Sidebar itemClickHandler={itemClickHandler} />
+          </div>
+          <div className="col-md-10 offset-md-2 py-5">
             <Routes>
               {AppRoutes.map((route) => (
                 <Route
