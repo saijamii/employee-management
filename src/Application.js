fe7d55c80e1073adfaba37012a5c4d7aec81f8fd
saijamii <saijami38@gmail.com>
2024-03-14 21:53:21 +0530

    path added

--- src/Application.js
+++ src/Application.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Department from "./Pages/Department/Department";
+import LeaveType from "./Pages/Leaves/LeaveType";
+import Employee from "./Pages/Employee/Employee";
+import Salary from "./Pages/Employee/Salary";
+import Request from "./Pages/Employee/Request";
+import Report from "./Pages/Report/Report";
 
 function Application() {
-  console.log("APPP");
-  return <div>Application</div>;
+  return (
+    <Routes>
+      <Route path="/app/dashboard" element={<Dashboard />} />
+      <Route path="/app/department" element={<Department />} />
+      <Route path="/app/leaveType" element={<LeaveType />} />
+      <Route path="/app/employee" element={<Employee />} />
+      <Route path="/app/salary" element={<Salary />} />
+      <Route path="/app/request" element={<Request />} />
+      <Route path="/app/report" element={<Report />} />
+    </Routes>
+  );
 }
 
 export default Application;
