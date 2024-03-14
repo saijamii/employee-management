ca90c0d91c7dae7af3324edb88d48c69803a00ec
saijamii <saijami38@gmail.com>
2024-03-14 22:22:44 +0530

    Basic Sider

--- src/SiderMenu.js
+++ src/SiderMenu.js
@@ -1,7 +1,48 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+const { Sider } = Layout;
 function SiderMenu() {
-  return <div>SiderMenu</div>;
+  const [collapsed, setCollapsed] = useState(false);
+  const items = [
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    AppstoreOutlined,
+    TeamOutlined,
+    ShopOutlined,
+  ].map((icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+  }));
+  return (
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={(value) => setCollapsed(value)}
+    >
+      <div className="demo-logo-vertical" />
+      <Menu
+        style={{ marginTop: "60px" }}
+        theme="dark"
+        defaultSelectedKeys={["1"]}
+        mode="inline"
+        items={items}
+      />
+    </Sider>
+  );
 }
 
 export default SiderMenu;
