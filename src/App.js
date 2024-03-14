847da0dbcbef292e92a46db68da81041907ced96
saijamii <saijami38@gmail.com>
2024-03-14 21:41:45 +0530

    Warninig

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Authenticated />} />
+        <Route path="*" element={<Authenticated />} />
       </Routes>
     </Router>
   );
