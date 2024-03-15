1c35d9107c888319f73b58d383ddff406789b068
saijamii <saijami38@gmail.com>
2024-03-15 05:53:27 +0530

    CommonHeader

--- src/CommonHeader.js
+++ src/CommonHeader.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
 
-function CommonHeader() {
-  return <div>CommonHeader</div>;
+export default function CommonHeader() {
+  return (
+    <>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+        <Menu.Item key="1">
+          <Link to="/">
+            <span>Home</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Link to="/contact">
+            <span>Contact</span>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </>
+  );
 }
-
-export default CommonHeader;
