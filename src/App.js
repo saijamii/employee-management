9d0401523191c8f42a25f2fd6eae9129d6352c66
saijamii <saijami38@gmail.com>
2024-03-15 07:07:34 +0530

    Authenticated path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Authenticated from "./Authenticated";
+import Authenticated from "./Common/Authenticated";
 
 export default function App() {
   return (
