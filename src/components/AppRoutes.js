f5a60717ef453f846b1fdebd56ffb82bb3f26a80
saijamii <saijami38@gmail.com>
2024-03-15 06:53:47 +0530

    title added in AppRoutes

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -10,13 +10,55 @@ import Login from "../Pages/Login/Login";
 const AppRoutes = [
   // { key: "0", exact: true, path: "/", component: Login },
   { key: "1", exact: true, path: "/login", component: Login },
-  { key: "2", exact: true, path: "/app/dashboard", component: Dashboard },
-  { key: "3", exact: true, path: "/app/department", component: Department },
-  { key: "4", exact: true, path: "/app/leaveType", component: LeaveType },
-  { key: "5", exact: true, path: "/app/employee", component: Employee },
-  { key: "6", exact: true, path: "/app/salary", component: Salary },
-  { key: "7", exact: true, path: "/app/request", component: Request },
-  { key: "8", exact: true, path: "/app/report", component: Report },
+  {
+    key: "2",
+    exact: true,
+    path: "/app/dashboard",
+    component: Dashboard,
+    title: "Dashboard",
+  },
+  {
+    key: "3",
+    exact: true,
+    path: "/app/department",
+    component: Department,
+    title: "Department",
+  },
+  {
+    key: "4",
+    exact: true,
+    path: "/app/leaveType",
+    component: LeaveType,
+    title: "LeaveType",
+  },
+  {
+    key: "5",
+    exact: true,
+    path: "/app/employee",
+    component: Employee,
+    title: "Employee",
+  },
+  {
+    key: "6",
+    exact: true,
+    path: "/app/salary",
+    component: Salary,
+    title: "Salary",
+  },
+  {
+    key: "7",
+    exact: true,
+    path: "/app/request",
+    component: Request,
+    title: "Request",
+  },
+  {
+    key: "8",
+    exact: true,
+    path: "/app/report",
+    component: Report,
+    title: "Report",
+  },
 ];
 
 export default AppRoutes;
