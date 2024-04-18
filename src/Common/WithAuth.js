e28f4c7f1a89bebe39b27faae5ec4f1ff1f1ede4
saijami <saijami@unosimple.com>
2024-04-18 17:29:29 +0530

    Revert "token changes"
    This reverts commit 44f5d788f16ee153694f06172a93b735bc592fef.

--- src/Common/WithAuth.js
+++ src/Common/WithAuth.js
@@ -2,13 +2,12 @@ import React from "react";
 import { useLocation, Navigate } from "react-router-dom";
 
 const WithAuth = (WrappedComponent) => {
-  const Authenticated = ({ ...props }) => {
+  const Authenticated = (props) => {
     const location = useLocation();
-    // const token = true;
-    const token = localStorage.getItem("token");
+    const token = true;
+    // const token = localStorage.getItem("token");
 
-    return (token !== undefined && token !== null && token !== "") ||
-      location.pathname === "/login" ? (
+    return token || location.pathname === "/login" ? (
       <WrappedComponent {...props} />
     ) : (
       <Navigate to="/login" />
